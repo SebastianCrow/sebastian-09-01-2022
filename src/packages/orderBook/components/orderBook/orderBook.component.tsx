7586b30ffd346dcfcf850b42b2f4a1fe9ca142c4
Sebastian Crow <sebastian@sebastiancrow.com>
2022-01-06 16:02:06 +0100

    PROJ-6 Layout for mobile and desktop

--- src/packages/orderBook/components/orderBook/orderBook.component.tsx
+++ src/packages/orderBook/components/orderBook/orderBook.component.tsx
@@ -1,24 +1,38 @@
 import React, { FunctionComponent } from 'react';
+import cns from 'classnames';
 import { SpreadHeader } from '../spreadHeader/spreadHeader.component';
+import styles from './orderBook.component.module.scss';
 
 // TODO: Replace all the inline styles with Sass
 
 export const OrderBook: FunctionComponent = () => {
   return (
-    <>
-      <div style={{ backgroundColor: 'red' }} className="desktop">
-        <div style={{ backgroundColor: 'green' }}>Order Book</div>
-        <div className="desktop">
+    <div className={styles.container}>
+      <div className={styles.header} style={{ backgroundColor: 'red' }}>
+        <div className={styles.title} style={{ backgroundColor: 'green' }}>
+          Order Book
+        </div>
+        <div className={cns(styles.spreadContainer, 'desktop')}>
           <SpreadHeader />
         </div>
       </div>
-      <div>
-        <div style={{ backgroundColor: 'orange' }} />
-        <div className="mobile">
+      <div className={styles.tableContainer}>
+        <div
+          className={styles.tableColumn}
+          style={{ backgroundColor: 'orange' }}
+        >
+          Column1
+        </div>
+        <div className={cns(styles.spreadContainer, 'mobile')}>
           <SpreadHeader />
         </div>
-        <div style={{ backgroundColor: 'orange' }} />
+        <div
+          className={styles.tableColumn}
+          style={{ backgroundColor: 'yellow' }}
+        >
+          Column2
+        </div>
       </div>
-    </>
+    </div>
   );
 };
